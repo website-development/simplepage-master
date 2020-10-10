2e39810606e4e9f3e140251e6f4796fe48672e2d
website-development <website.dewelopment@gmail.com>
2020-10-10 16:42:10 +0300

    Updates

--- src/components/button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import styles from './button.module.scss'
-import classnames from 'classnames'
-
-export const Button = ({title, positive}) => (
-    <button className={classnames(styles.button, positive && styles.positive)}>{title}</button>
-)
